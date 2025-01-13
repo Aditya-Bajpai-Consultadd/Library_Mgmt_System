303a022c31e7abc787531d7405dcfb529382b371
consultadd <consultadd@consultadds-MacBook-Pro.local>
2025-01-13 08:33:17 +0530

    added comment

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.secret_key="something something"
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+#git changes
 db = SQLAlchemy(app)
 loginManager = LoginManager()
 loginManager.init_app(app)
